2293b49811f6e84f313c518a35104fb04e6908e8
Remco Janssen <entreco@gmail.com>
2019-01-09 23:50:29 +0100

    # Dynamic Spring swiping

--- android/viewer/src/main/java/nl/entreco/giddyapp/viewer/swiper/SwipeHorseView.kt
+++ android/viewer/src/main/java/nl/entreco/giddyapp/viewer/swiper/SwipeHorseView.kt
@@ -8,6 +8,8 @@ import android.widget.FrameLayout
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
 import nl.entreco.giddyapp.viewer.databinding.WidgetSwipeHorseViewBinding
+import kotlin.math.PI
+import kotlin.math.abs
 
 class SwipeHorseView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -22,25 +24,58 @@ class SwipeHorseView @JvmOverloads constructor(
         binding = WidgetSwipeHorseViewBinding.inflate(inflater, this, true)
     }
 
-    private var halfTheScreenWidth = 0
+    private var swipeProgess = 0F
+    private var screenWidth = 0
     private var viewOffsetFactor = 0
+    private val piAsFloat = PI.toFloat()
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        halfTheScreenWidth = context.resources.displayMetrics.widthPixels / 2
-        viewOffsetFactor = w / 2 - halfTheScreenWidth
+        screenWidth = context.resources.displayMetrics.widthPixels
+        viewOffsetFactor = w / 2 - screenWidth / 2
     }
 
     private val onDragged: (Float, Float) -> Unit = { x, y ->
-        if (viewOffsetFactor != 0 && halfTheScreenWidth != 0) {
-            Log.d("DRAG", "progress: ${((x + viewOffsetFactor) / halfTheScreenWidth)}")
+        if (viewOffsetFactor != 0 && screenWidth != 0) {
             springX.setStartValue(x)
             springY.setStartValue(y)
+            swipeProgess = (x + viewOffsetFactor) / screenWidth
+            Log.d("DRAG", "progress: $swipeProgess")
+            someRandomDepthTransformation(swipeProgess)
         }
     }
 
-    private val onReleased: (Float, Float) -> Unit = { x, y ->
-        Log.d("DRAG", "released: $x, $y")
+    private fun someRandomDepthTransformation(progress: Float) {
+        scaleX = 1 - (abs(progress) / 6F)
+        scaleY = 1 - (abs(progress) / 6F)
+        translationZ = abs(progress) * 8 + 2F
+        rotationY = progress * piAsFloat
+    }
+
+    private val onReleased: (Float, Float) -> Unit = { startX, startY ->
+        Log.d("DRAG", "released: $startX, $startY")
+
+        // TODO: Check 3 cases:
+        // - User liked Horse -> dismiss and load next
+        // - User Disliked Horse -> dismiss and load next
+        // - User Cancelled ->
+        when {
+            swipeProgess < -.5 -> {
+            }
+            swipeProgess > .5 -> {
+            }
+            else -> {
+            }
+        }
+
+        onUserCancelled(startX, startY)
+        swipeProgess = 0F
+    }
+
+    private fun onUserCancelled(x: Float, y: Float) {
+        // Reset all properties changed by 'someRandomDepthTransformation'
+        animate().scaleY(1F).scaleX(1F).rotationY(0F).translationZ(2F).setDuration(100).start()
+        // Release the Springs
         springX.apply {
             animateToFinalPosition(x)
         }
