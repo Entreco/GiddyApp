917d23bc036d5b44e8031aef7db4979fbb90ebe8
Remco Janssen <entreco@gmail.com>
2019-08-03 15:51:42 +0200

    #35: Update To AS3.5 beta

--- android/libraries/libCore/src/main/java/nl/entreco/giddyapp/libcore/launch/LaunchHelper.kt
+++ android/libraries/libCore/src/main/java/nl/entreco/giddyapp/libcore/launch/LaunchHelper.kt
@@ -53,14 +53,10 @@ object LaunchHelper {
         context
     )
 
-    private fun baseIntent(url: String, context: Context? = null): Intent {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            .addCategory(Intent.CATEGORY_DEFAULT)
+    private fun baseIntent(url: String, context: Context): Intent {
+        return Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            .setPackage(context.packageName)
             .addCategory(Intent.CATEGORY_BROWSABLE)
-        if (context != null) {
-            intent.`package` = context.packageName
-        }
-        return intent
     }
 
     private fun launch(intent: Intent, options: ActivityOptions?, activity: Activity) {
